fa2627028569fefec913dd3245233a835ca71d6f
Augustin Jose <44978968+AugustinJose1221@users.noreply.github.com>
2020-08-05 23:20:20 +0530

    Update Handler.py

--- api/Handler.py
+++ api/Handler.py
@@ -9,31 +9,20 @@ Created on Wed Aug  5 10:40:17 2020
 
 from flask import Flask, Response, jsonify, render_template, request, redirect, url_for
 import tweepy as tw
+import requests
+from base64 import b64encode
 
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
-from GrabzIt import GrabzItImageOptions
-from GrabzIt import GrabzItClient
 
 import os
 import random
-
 consumer_key = str(os.getenv("CONSUMER_KEY"))
 consumer_secret = str(os.getenv("CONSUMER_SECRET"))
 key = str(os.getenv("ACCESS_KEY"))
 secret = str(os.getenv("ACCESS_SECRET"))
 username = str(os.getenv("USERNAME"))
-
-grazit_key = str(os.getenv("GRAZIT_KEY"))
-grazit_token = str(os.getenv("GRAZIT_TOKEN"))
-
-grabzIt = GrabzItClient.GrabzItClient(grazit_key, grazit_token)
-
-options = GrabzItImageOptions.GrabzItImageOptions()
-options.format = "png"
-options.targetElement = "#features"
-
 
 app = Flask(__name__)
 
@@ -48,22 +37,40 @@ def TweetInfo(target):
     conn = OAuth()
     for status in tw.Cursor(conn.user_timeline, id=target).items(1):
         pass
-    return status.id
+    return [status.text, status.entities, status.created_at]
     
-        
+def loadImageB64(url):
+    resposne = requests.get(url)
+    return b64encode(resposne.content).decode("ascii")
+
 def makeTweet():
-    ID = TweetInfo(username)
-    html = "<div id=\"features\"><blockquote class=\"twitter-tweet\"><a href=\"https://twitter.com/"+username+"/status/"+str(ID)+"\"></a></blockquote> <script async src=\"https://platform.twitter.com/widgets.js\" charset=\"utf-8\"></script> </div>"
-    grabzIt.HTMLToImage(html, options)
-    #grabzIt.SaveTo("result.png")
-    #html = "https://twitter.com/"+username+"/status/"+str(ID)
-    return grabzIt#render_template("tweet.html.j2", link=html)
+    details = TweetInfo(username)
+    time = details[2]
+    tweet = details[0]
+    hashtag = details[1]["hashtags"]
+    hashtags = []
+    for i in hashtag:
+        hashtags.append("#"+str(i["text"]))
+    user_mention = details[1]["user_mentions"]
+    user_mentions = []
+    for j in user_mention:
+        user_mentions.append("@"+str(j["screen_name"]))
+    
+    for i in hashtags:
+        tweet = tweet.replace(i, "<span>"+str(i)+"</span>")
+    for i in user_mentions:
+        tweet = tweet.replace(i, "<span>"+str(i)+"</span>")
+        
+    top = loadImageB64("https://raw.githubusercontent.com/AugustinJose1221/ReadmeBot/master/api/templates/top.png")
+    bottom = loadImageB64("https://raw.githubusercontent.com/AugustinJose1221/ReadmeBot/master/api/templates/bottom.png")
+    return render_template("tweet.html.j2", top=top, bottom=bottom, tweettext=tweet, date_time=time)
 
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
 def catch_all(path):
+    
     svg=makeTweet()
-    resp = Response(svg, mimetype="image/jpeg")
+    resp = Response(svg, mimetype="image/svg+xml")
     resp.headers["Cache-Control"] = "s-maxage=1"
     return resp
 
